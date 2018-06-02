2dbfe5dac71ad71c2337605003aed6d28f440583
Gsllchb <Gsllchb@gmail.com>
2018-06-02 14:33:10 +0800

    rename AreOverlapping to areOverlapping

--- src/RectanglesOverlapping.java
+++ src/RectanglesOverlapping.java
@@ -12,7 +12,7 @@ public class RectanglesOverlapping {
     }
   }
 
-  public static boolean AreOverlapping(Rectangle r1, Rectangle r2) {
+  public static boolean areOverlapping(Rectangle r1, Rectangle r2) {
     if (r1 == null || r2 == null) {
       return false;
     }
@@ -26,57 +26,57 @@ public class RectanglesOverlapping {
   public static void main(String[] args) {
     Rectangle r1 = new Rectangle(0, 0, 1, 1);
     Rectangle r2 = new Rectangle(2, 2, 1, 1);
-    assert !AreOverlapping(r1, r2);
-    assert !AreOverlapping(r2, r1);
+    assert !areOverlapping(r1, r2);
+    assert !areOverlapping(r2, r1);
 
     r1 = new Rectangle(0, 0, 1, 1);
     r2 = new Rectangle(1, 0, 1, 1);
-    assert !AreOverlapping(r1, r2);
-    assert !AreOverlapping(r2, r1);
+    assert !areOverlapping(r1, r2);
+    assert !areOverlapping(r2, r1);
 
     r1 = new Rectangle(-2, -2, 4, 4);
     r2 = new Rectangle(-1, -1, 2, 2);
-    assert AreOverlapping(r1, r2);
-    assert AreOverlapping(r2, r1);
+    assert areOverlapping(r1, r2);
+    assert areOverlapping(r2, r1);
 
     r1 = new Rectangle(0, 0, 2, 2);
     r2 = new Rectangle(1, 1, 2, 2);
-    assert AreOverlapping(r1, r2);
-    assert AreOverlapping(r2, r1);
+    assert areOverlapping(r1, r2);
+    assert areOverlapping(r2, r1);
 
     r1 = new Rectangle(0, 0, 2, 1);
     r2 = new Rectangle(1, 0, 2, 1);
-    assert AreOverlapping(r1, r2);
-    assert AreOverlapping(r2, r1);
+    assert areOverlapping(r1, r2);
+    assert areOverlapping(r2, r1);
 
     r1 = new Rectangle(-2, -1, 4, 2);
     r2 = new Rectangle(-1, -2, 2, 4);
-    assert AreOverlapping(r1, r2);
-    assert AreOverlapping(r2, r1);
+    assert areOverlapping(r1, r2);
+    assert areOverlapping(r2, r1);
 
     r1 = new Rectangle(-2, -1, 4, 2);
     r2 = new Rectangle(-1, -1, 2, 4);
-    assert AreOverlapping(r1, r2);
-    assert AreOverlapping(r2, r1);
+    assert areOverlapping(r1, r2);
+    assert areOverlapping(r2, r1);
 
     r1 = new Rectangle(-2, -1, 4, 2);
     r2 = new Rectangle(-1, 0, 2, 4);
-    assert AreOverlapping(r1, r2);
-    assert AreOverlapping(r2, r1);
+    assert areOverlapping(r1, r2);
+    assert areOverlapping(r2, r1);
 
     r1 = new Rectangle(0, 0, 2, 2);
     r2 = new Rectangle(1, 1, 0, 0);
-    assert AreOverlapping(r1, r2);
-    assert AreOverlapping(r2, r1);
+    assert areOverlapping(r1, r2);
+    assert areOverlapping(r2, r1);
 
     r1 = new Rectangle(0, 0, 0, 0);
     r2 = new Rectangle(1, 1, 0, 0);
-    assert !AreOverlapping(r1, r2);
-    assert !AreOverlapping(r2, r1);
+    assert !areOverlapping(r1, r2);
+    assert !areOverlapping(r2, r1);
 
     r1 = null;
     r2 = new Rectangle(2, 2, 1, 1);
-    assert !AreOverlapping(r1, r2);
-    assert !AreOverlapping(r2, r1);
+    assert !areOverlapping(r1, r2);
+    assert !areOverlapping(r2, r1);
   }
 }
